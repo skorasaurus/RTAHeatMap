2be2736c0a2a8cec5fd7b40268a37085aef3b216
Will Skora <skorasaurus@gmail.com>
2016-05-27 16:52:25 -0400

    add context path and init for tests

---
+++ DataGeneration/tests/context.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# http://www.kennethreitz.org/essays/repository-structure-and-python
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# this is the folder of modules
+import DataGeneration
